11f7b9cc0aed10c875580c19dd9ffb284f900d68
akashshah10 <208366876+akashshah10@users.noreply.github.com>
2026-03-20 17:41:00 +0530

    Right angled triangle pattern using numbers

--- Patterns/main.cpp
+++ Patterns/main.cpp
@@ -27,11 +27,25 @@ void p2(int n)
     }
 }
 
+//Right angled triangle pattern using numbers
+void p3(int n)
+{
+    for(int i = 1; i <= n; i++) 
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int n;
     cin >> n;
     p1(n);
     p2(n);
+    p3(n);
     return 0;
 }
