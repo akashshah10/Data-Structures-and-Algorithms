e688fbdc1d86742252b66e1ce2ea2c40f69481a3
akashshah10 <208366876+akashshah10@users.noreply.github.com>
2026-03-28 13:17:28 +0530

    Iterators

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -4,10 +4,16 @@ using namespace std;
 
 int main()
 {
-    vector<int> v = {4,5};
-    cout << v.size() << endl;
-    v.push_back(1);
-    cout << v.size();
+    vector<int> v = {4,5,6,7};
+    // cout << v.size() << endl;
+    // v.push_back(1);
+    // cout << v.size();
+    for(vector<int>::iterator it = v.begin(); it != v.end(); it++)
+    {
+        cout << *(it) << " ";
+    }
+    cout << endl;
+
    
     return 0;
 }
