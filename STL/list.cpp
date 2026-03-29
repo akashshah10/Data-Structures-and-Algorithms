3079442f5560948538e9a97bc377d13870854faa
akashshah10 <208366876+akashshah10@users.noreply.github.com>
2026-03-29 11:23:40 +0530

    Methods of List

--- STL/list.cpp
+++ STL/list.cpp
@@ -1,1 +1,25 @@
-//List is just like vectors but it also gives you front operations as well
+//std::list is a doubly linked list that allows efficient insertion and deletion from both ends and anywhere, but it does not support random access like std::vector.
+#include <iostream>
+#include <list>  //Header for list
+using namespace std;
+
+void expList()
+{
+    list<int> l;
+
+    l.push_back(4);
+    l.emplace_back(6);
+
+    l.push_front(5);
+    l.emplace_front(10);
+    for(auto it = l.begin(); it != l.end(); it++)
+    {
+        cout << *(it) <<" ";
+    }
+}
+
+int main()
+{
+    expList();
+    return 0;
+}
