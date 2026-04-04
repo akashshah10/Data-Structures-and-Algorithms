09b93e6862470e950db9f49b7138dbc9ca96c0a3
akashshah10 <208366876+akashshah10@users.noreply.github.com>
2026-04-04 17:52:25 +0530

    Added check condition for overflow/underflow

--- Basic Maths/reverse.cpp
+++ Basic Maths/reverse.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
 int reverseNumber(int n)
@@ -6,7 +7,9 @@ int reverseNumber(int n)
     int revNum = 0;
     while(n != 0)
     {
-        revNum = (revNum * 10) + (n % 10);
+        int digit = n % 10;
+        if(revNum > INT_MAX / 10|| revNum < INT_MIN / 10) return 0;
+        revNum = (revNum * 10) + digit;
         n /= 10;
     }
     return revNum;
