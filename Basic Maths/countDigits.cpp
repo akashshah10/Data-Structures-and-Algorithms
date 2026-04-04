60f8b51efcd3e6e18a56be5bcf2295d61f95d297
akashshah10 <208366876+akashshah10@users.noreply.github.com>
2026-04-04 13:05:52 +0530

    Optimal approach for counting the digits

--- Basic Maths/countDigits.cpp
+++ Basic Maths/countDigits.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int countDigit(int n)
-{
-    int count = 0;
-    while(n > 0)
-    {
-        count ++;
-        n /= 10;
-    }
-    return count;
-}
+// //Brute Force Approach
+// int countDigit(int n)
+// {
+//     int count = 0;
+//     while(n > 0)
+//     {
+//         count ++;
+//         n /= 10;
+//     }
+//     return count;
+// }
 
+
+//Optimal Approach
+int countDigit2(int n)
+{
+    if(n == 0) return 0;
+    return (int)(log10(abs(n))) + 1;  //abs gives the absolute value of the number
+}
 int main()
 {
     int n;
     cout << "Enter the number: " << endl;
     cin >> n;
-    cout << "Number of digits: " << countDigit(n);
+    cout << "Number of digits: " << countDigit2(n);
     return 0;
 }
