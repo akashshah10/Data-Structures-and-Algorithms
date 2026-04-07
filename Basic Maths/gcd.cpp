e63c8ef2ca6a563e5de900c0bfe0e61fe520eac1
akashshah10 <208366876+akashshah10@users.noreply.github.com>
2026-04-07 12:25:12 +0530

    Calculating GCD- Brute force

---
+++ Basic Maths/gcd.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int calculateGCD(int n1, int n2)
+{
+    int GCD = 1;
+    for(int i = 1; i <= min(n1,n2); i++)
+        {
+            if(n1 % i == 0 && n2 % i == 0)
+            {
+                GCD = i;
+            }
+        }
+        return GCD;
+}
+
+int main()
+{
+    int n1, n2;
+    cout << "Enter the first number: " << endl;
+    cin >> n1;
+    cout << "Enter the second number: " << endl;
+    cin >> n2;
+    cout << "The GCD of given numbers is: " << calculateGCD(n1,n2);
+
+    return 0;
+}
