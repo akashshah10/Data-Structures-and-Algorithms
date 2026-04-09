bf62a9fe0711810752210e9b744ac5167458d837
akashshah10 <208366876+akashshah10@users.noreply.github.com>
2026-04-09 19:43:09 +0530

    Print numbers n to 1

--- Recursion/PrintNumbers.cpp
+++ Recursion/PrintNumbers.cpp
@@ -4,10 +4,14 @@ using namespace std;
 void printNumbers(int n)
 {
     //1 2 3 4
-    if(n < 1)return;
-    printNumbers(n - 1);
-    cout << n << " ";
+    // if(n < 1)return;
+    // printNumbers(n - 1);
+    // cout << n << " ";
 
+    //4 3 2 1
+    if(n < 1)return;
+    cout << n << " ";
+    printNumbers(n - 1);
 }
 
 int main()
