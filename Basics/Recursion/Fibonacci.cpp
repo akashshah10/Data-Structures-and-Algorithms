0009d200a103851bc252b914a3135de9a955b681
akashshah10 <208366876+akashshah10@users.noreply.github.com>
2026-04-23 15:31:38 +0530

    Fibonacci using recursion

---
+++ Basics/Recursion/Fibonacci.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int calFibonacci(int n)
+{
+    if(n == 0)return 0;
+    if(n == 1)return 1;
+    return calFibonacci(n-1) + calFibonacci(n-2);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << calFibonacci(n);
+    return 0;
+}
+
