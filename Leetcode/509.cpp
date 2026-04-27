bd163af09efc4982e290346561513133eaf11c47
akashshah10 <208366876+akashshah10@users.noreply.github.com>
2026-04-27 14:02:23 +0530

    Initial commit

---
+++ Leetcode/509.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int calFibonacci(int n)
+{
+    if(n <= 1)return n;
+
+    int prev1 = 1, prev2 = 0, curr = 0;
+    for(int i = 2; i <= n;i ++)
+    {
+        curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return curr;
+}
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << calFibonacci(n);
+    return 0;
+}
