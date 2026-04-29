ad3fe6e5a209cf896c00f0dc3565e86fc1d35041
akashshah10 <208366876+akashshah10@users.noreply.github.com>
2026-04-29 18:40:43 +0530

    Frequecy using map

---
+++ Basics/Maps/map.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    int arr[n];
+
+    cout << "Enter the elements of array: ";
+    for(int i = 0; i < n; i ++)
+    {
+        cin >> arr[i];
+    }
+
+    map<int, int>freq;
+    for(int i = 0; i < n; i++)
+    {
+        freq[arr[i]]++;
+    }
+
+    for(auto it: freq)
+    {
+        cout << it.first << "-->" << it.second << endl;
+    }
+
+    int q;
+    cout << "Enter the number of queries: ";
+    cin >> q;
+    while(q--)
+    {
+        int numbers;
+        cin >> numbers;
+        cout << freq[numbers] << endl;
+    }
+    return 0;
+}
